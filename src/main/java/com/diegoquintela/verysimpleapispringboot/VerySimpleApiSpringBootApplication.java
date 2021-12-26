e9d025026f6c3e4be338170b375cd6836d3485ed
Diego Quintela <diegolucasq@gmail.com>
2021-12-26 18:42:28 -0500

    initial commit

---
+++ src/main/java/com/diegoquintela/verysimpleapispringboot/VerySimpleApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.diegoquintela.verysimpleapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerySimpleApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerySimpleApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegoquintela/verysimpleapispringboot/VerySimpleApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoquintela.verysimpleapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerySimpleApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
